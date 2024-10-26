c8d4518ee6f6b307bbddd06fa8257db8f8f5b552
pillypenkosa <pillypenkosa@gmail.com>
2024-10-27 00:08:17 +0300

    Media 2024-10-26 23:45

--- component/menu-filter-people/cmp.menu-filter-people.js
+++ component/menu-filter-people/cmp.menu-filter-people.js
@@ -41,126 +41,21 @@ class ComponentMenuFilterPeople {
 
  		let optionCat = '<option value="all">- Всі ---</option>';
  		arrMoviePeople.forEach( k => {
-			optionCat += `<option value="${ k.id }">${ k.title }</option>`;
- 		});
-
-
-
-
-/*
-
-
- 		let optionActor = '<option value="all">- Актори ---</option>';
-		arrFilmsActors.sort().forEach( k => {
 
  			let attrSelected = '';
-			let name = '';
 	 		if ( Router.urlGET ) {
-	 			if ( Router.urlGET.win == 'movies' ) {
-	 				if ( Router.urlGET.cast ) {
-			 			if ( k == Router.urlGET.cast ) 
-			 				attrSelected = 'selected';
+	 			if ( Router.urlGET.win == 'people' ) {
+	 				if ( Router.urlGET.cat ) {
+	 					if ( k.id ) {
+				 			if ( k.id == Router.urlGET.cat ) 
+				 				attrSelected = 'selected';
+	 					}
 	 				}
 	 			}
 	 		}
 
-			if ( objListPeople[ k ] ) {
-				if ( objListPeople[ k ].name ) {
-
-					if ( objListPeople[ k ].name.n ) 
-						name += objListPeople[ k ].name.n;
-
-					if ( objListPeople[ k ].name.s ) 
-						name += ' ' + objListPeople[ k ].name.s;
-				} 
-			}
-
-		 	optionActor += `<option value="${ k }" ${ attrSelected }>${ name }</option>`;
-		});
-
-
-
-
-
- 		let optionDirectors = '<option value="all">- Режисери ---</option>';
-		arrFilmsDirectors.sort().forEach( k => {
-
- 			let attrSelected = '';
-			let name = '';
-	 		if ( Router.urlGET ) {
-	 			if ( Router.urlGET.win == 'movies' ) {
-	 				if ( Router.urlGET.director ) {
-			 			if ( k == Router.urlGET.director ) 
-			 				attrSelected = 'selected';
-			 		}
-			 	}
-			}
-
-			if ( objListPeople[ k ] ) {
-
-				if ( objListPeople[ k ].name.n ) 
-					name += objListPeople[ k ].name.n;
-
-				if ( objListPeople[ k ].name.s ) 
-					name += ' ' + objListPeople[ k ].name.s;
-			}
-
-		 	optionDirectors += `<option value="${ k }" ${ attrSelected }>${ name }</option>`;
-		});
-
-
-
-
-
- 		let optionComposer = '<option value="all">- Композитори ---</option>';
-		arrFilmsComposers.sort().forEach( k => {
-
- 			let attrSelected = '';
-			let name = '';
-	 		if ( Router.urlGET ) {
-	 			if ( Router.urlGET.win == 'movies' ) {
-	 				if ( Router.urlGET.composer ) {
-			 			if ( k == Router.urlGET.composer ) 
-			 				attrSelected = 'selected';
-			 		}
-			 	}
-			}
-
-			if ( objListPeople[ k ] ) {
-
-				if ( objListPeople[ k ].name.n ) 
-					name += objListPeople[ k ].name.n;
-
-				if ( objListPeople[ k ].name.s ) 
-					name += ' ' + objListPeople[ k ].name.s;
-			}
-
-		 	optionComposer += `<option value="${ k }" ${ attrSelected }>${ name }</option>`;
-		});
-
-
-*/
-
-
-
-/*
-
-	 			<div>
-	 				<select data-id="actor" onchange="${ this.name }.changeFilter( this )">${ optionActor }</select>
-	 			</div>
-	 			<div>
-	 				<select data-id="director" onchange="${ this.name }.changeFilter( this )">${ optionDirectors }</select>
-	 			</div>
-	 			<div>
-	 				<select data-id="composer" onchange="${ this.name }.changeFilter( this )">${ optionComposer }</select>
-	 			</div>
-
-*/
-
-
-
-
-
+			optionCat += `<option value="${ k.id }" ${ attrSelected }>${ k.title }</option>`;
+ 		});
 
 
 
@@ -199,28 +94,12 @@ class ComponentMenuFilterPeople {
 		//console.log( 'elem: ', elem ); 
 
 
-		//console.log( 'elem-ID: ', elem.dataset.id ); 
-		//console.log( 'value: ', elem.value ); 
-
-
-		// скидання фільтрів
-		//ComponentMenu.resetFilter();
-		// приховування меню
-		//ComponentHeader.clc();
-
 		Router.urlGET = {};
-
 
 		Router.link([
 			{ k: 'win' 	, v: 'people' 		, },
 			{ k: 'cat' 	, v: elem.value 	, },
 		]);
-
-
-
-
-
- 
 	} 
  
  
