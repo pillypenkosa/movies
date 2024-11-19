429c7ffa62307d5c8a49c8c673e09a000840bf16
pillypenkosa <pillypenkosa@gmail.com>
2024-11-19 17:55:19 +0200

    Movies 2024-11-19 17:50

--- list/list.films.js
+++ list/list.films.js
@@ -1638,6 +1638,11 @@ const arrFilms = [
 	cast: { nina_siemaszko_14071970: 1, },
 },
 {
+	id: 'the_wild_robot_2024', title: { ua: 'Дикий робот', en: 'The Wild Robot', }, year: 2024, country: { usa: 1, jpn: 1, },
+	genre: { cartoon: 1, },	
+	internet: { imdb: '29623480', wiki_ua: '%D0%94%D0%B8%D0%BA%D0%B8%D0%B9_%D1%80%D0%BE%D0%B1%D0%BE%D1%82_(%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%84%D1%96%D0%BB%D1%8C%D0%BC)', },
+},
+{
 	id: 'kindergarten_cop_1990', title: { ua: 'Дитсадковий поліцейський', en: 'Kindergarten Cop', }, year: 1990, country: { usa: 1, }, rating: 5,
 	genre: { action: 1, comedy: 1, crime: 1, }, hash: { police: 1, children: 1, },
 	internet: { imdb: '0099938', wiki_ua: '%D0%94%D0%B8%D1%82%D1%81%D0%B0%D0%B4%D0%BA%D0%BE%D0%B2%D0%B8%D0%B9_%D0%BF%D0%BE%D0%BB%D1%96%D1%86%D0%B5%D0%B9%D1%81%D1%8C%D0%BA%D0%B8%D0%B9', },
@@ -2548,7 +2553,7 @@ const arrFilms = [
 },
 {
 	id: 'influencer_2022', title: { ua: 'Інфлюенсер', en: 'Influencer', }, year: 2022, country: { usa: 1, },
-	genre: { thriller: 1, horror: 1, },
+	genre: { thriller: 1, crime: 1, }, hash: { island: 1, },
 	internet: { imdb: '13309170', },	
 },
 {
